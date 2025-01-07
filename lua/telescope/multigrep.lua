745adcb0b1f71284281ff2280f3afd28a19481d1
Jono Mason <60683297+MassoniMacaroni@users.noreply.github.com>
2025-01-07 19:28:27 +1100

    add desc to leader fg

--- lua/telescope/multigrep.lua
+++ lua/telescope/multigrep.lua
@@ -49,7 +49,7 @@ local live_multigrep = function(opts)
 end
 
 M.setup = function()
-  vim.keymap.set('n', '<leader>fg', live_multigrep)
+  vim.keymap.set('n', '<leader>fg', live_multigrep, { desc = '[f]ind with [g]rep' })
 end
 
 return M

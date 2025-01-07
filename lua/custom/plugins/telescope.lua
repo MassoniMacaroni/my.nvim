f4bce7c10ed488d8a1340b784cec263b3692f417
Jono Mason <60683297+MassoniMacaroni@users.noreply.github.com>
2025-01-07 19:23:19 +1100

    add ripgrep telescope support

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -101,6 +101,8 @@ return {
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
+      require('telescope.multigrep').setup()
     end,
   },
 }
